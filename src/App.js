7fdcb4112633ea60344a24568e74040495576708
shauek97 <shauek97@gmail.com>
2023-06-20 11:56:01 +0200

    Installed react icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/Navbar'
 import WelcomeSection from './components/WelcomeSection/WelcomeSection';
+import About from './components/About/About';
 
 function App() {
   return (
     <div>
       <Navbar />
       <WelcomeSection />
+      <About />
     </div>
   );
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,49 @@
-import styles from './about.module.scss'
-import Container from '../Container/Container';
-import Menu from './Menu';
-import MobileMenu from './MobileMenu';
+import styles from './About.module.scss';
+import Container from "../Container/Container";
 
-export default function Navbar() {
-    return (
+export default function About() {
+  return (
+    <Container>
         <section className={styles.about}>
+        <div className={styles.title}>
+        <h2>TO NAS WYRÓŻNIA</h2>
+        </div>
+        <div className={styles.mainBox}>
+            <div className={styles.attribute}>
+                <div className={styles.attributeTitle}>
+                    <h3>Przyjazna atmosfera</h3>
+                </div>
+                <div className={styles.attributeIcon}>
 
+                </div>
+                <div className={styles.attributeDescription}>
+                    
+                </div>
+            </div>
+            <div className={styles.attribute}>
+                <div className={styles.attributeTitle}>
+                    <h3>Nowe samochody</h3>
+                </div>
+                <div className={styles.attributeIcon}>
+
+                </div>
+                <div className={styles.attributeDescription}>
+                    
+                </div>
+            </div>
+            <div className={styles.attribute}>
+                <div className={styles.attributeTitle}>
+                    <h3>Atrakcyjne wynagrodzenie</h3>
+                </div>
+                <div className={styles.attributeIcon}>
+
+                </div>
+                <div className={styles.attributeDescription}>
+                    
+                </div>
+            </div>
+        </div>
         </section>
-    );
-  }
+    </Container>
+  );
+}
